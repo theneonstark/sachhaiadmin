375d9e2c04e721aa6617ab38b3dc18bbd2bda5a8
theneonstark <mohd47149@gmail.com>
2025-11-17 16:46:50 +0530

    build update

--- public/build/assets/index-DzxNtijP.js
+++
@@ -1,1 +0,0 @@
-import{e as r,g as t}from"./app-CPP3CGEh.js";var a=r();const o=t(a);export{o as R,a as r};
---
+++ public/build/assets/index-RQCREH6P.js
@@ -0,0 +1,1 @@
+import{f as r,g as t}from"./app-Dl9O7i2l.js";var a=r();const e=t(a);export{e as R,a as r};
